0e9fbaed8edd5a979e96ba1660a6bcef5bbc4763
mementox <sebestyen.mate98@gmail.com>
2020-04-17 21:28:38 +0200

    Initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: .maven/bin/mvn -pl api -am spring-boot:run
+communicator: .maven/bin/mvn -pl communicator -am spring-boot:run
---
+++ communicator/src/main/java/zzleep/communicator/App.java
@@ -0,0 +1,22 @@
+package zzleep.communicator;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        while (true) {
+            Thread.sleep(5000);
+            System.out.println("Communicator is running...");
+        }
+    }
+}

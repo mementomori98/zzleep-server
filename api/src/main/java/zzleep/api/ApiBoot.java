e585b75a720ccaad6eb0751de8729433fdccd19b
mementox <sebestyen.mate98@gmail.com>
2020-04-22 17:38:00 +0200

    Fix configurations

--- api/src/main/java/zzleep/api/ApiBoot.java
+++ api/src/main/java/zzleep/api/ApiBoot.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("zzleep")
+@ComponentScan("zzleep.core")
 public class ApiBoot
 {
     public static void main( String[] args )

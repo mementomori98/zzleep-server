3d97c66f369dcdaeb6539d35fdff589fd6ddcace
mementox <sebestyen.mate98@gmail.com>
2020-04-22 18:04:15 +0200

    Fix startup error

--- api/src/main/java/zzleep/api/ApiBoot.java
+++ api/src/main/java/zzleep/api/ApiBoot.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("zzleep.core")
+@ComponentScan("zzleep")
 public class ApiBoot
 {
     public static void main( String[] args )

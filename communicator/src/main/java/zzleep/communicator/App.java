6582fd74a67c5774eff71b7314667da6194ff211
mementox <sebestyen.mate98@gmail.com>
2020-04-27 16:01:41 +0200

    No changes

--- communicator/src/main/java/zzleep/communicator/App.java
+++ communicator/src/main/java/zzleep/communicator/App.java
@@ -3,8 +3,10 @@ package zzleep.communicator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("zzleep")
 public class App implements CommandLineRunner
 {
     public static void main( String[] args )

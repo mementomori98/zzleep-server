9cb0ef1f9e07212cb8c71b2eb975de14d0240c0a
mementox <sebestyen.mate98@gmail.com>
2020-05-25 16:10:27 +0200

    Fix authentication and buy xanax

--- api/src/main/java/zzleep/api/config/SecurityConfig.java
+++ api/src/main/java/zzleep/api/config/SecurityConfig.java
@@ -2,59 +2,50 @@ package zzleep.api.config;
 
 // import mox.bigtalk.security.BasicAuthenticationEndpointImpl;
 // import mox.bigtalk.security.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import zzleep.api.security.BasicAuthenticationEndpointImpl;
 import zzleep.api.security.UserDetailsServiceImpl;
 
-import java.util.Arrays;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    private final BasicAuthenticationEndpointImpl authenticationEntryPoint;
-    private final UserDetailsServiceImpl userDetailsService;
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
-    public SecurityConfig(
-        BasicAuthenticationEndpointImpl authenticationEntryPoint,
-        UserDetailsServiceImpl userDetailsService
-    ) {
-        this.authenticationEntryPoint = authenticationEntryPoint;
-        this.userDetailsService = userDetailsService;
+        private final UserDetailsServiceImpl userDetailsService;
+
+        public ApiWebSecurityConfigurationAdapter(UserDetailsServiceImpl userDetailsService) {
+            this.userDetailsService = userDetailsService;
+        }
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                .antMatcher("/api/**")
+                .authorizeRequests(a -> a.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic()
-            .authenticationEntryPoint(authenticationEntryPoint);
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                .antMatcher("/swagger-ui.html")
+                .authorizeRequests(a -> a.anyRequest().permitAll());
+        }
     }
 }

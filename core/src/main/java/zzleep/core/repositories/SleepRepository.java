9ac759eb08decbbdaacbbaa5743ae966d061d99a
mementox <sebestyen.mate98@gmail.com>
2020-05-27 16:06:57 +0200

    TrackingController cleanup + auth, sleepRepo cleanup

--- core/src/main/java/zzleep/core/repositories/SleepRepository.java
+++ core/src/main/java/zzleep/core/repositories/SleepRepository.java
@@ -1,17 +1,15 @@
 package zzleep.core.repositories;
 
-import javassist.NotFoundException;
-import org.springframework.stereotype.Component;
 import zzleep.core.models.Sleep;
 
 public interface SleepRepository {
-    Sleep rateSleep(String sleepId, int rating) throws SleepNotFoundException;
+    Sleep rateSleep(int sleepId, int rating) throws SleepNotFoundException;
     Sleep startTracking(String deviceId) throws SleepNotStoppedException, DeviceNotFoundException;
     Sleep stopTracking(String deviceId) throws SleepNotStartedException, DeviceNotFoundException;
     boolean isTracking(String deviceId);
 
-    class SleepNotStoppedException extends Exception{}
-    class SleepNotStartedException extends Exception{}
-    class SleepNotFoundException extends Exception{}
-    class DeviceNotFoundException extends Exception{}
+    class SleepNotStoppedException extends RuntimeException{}
+    class SleepNotStartedException extends RuntimeException{}
+    class SleepNotFoundException extends RuntimeException{}
+    class DeviceNotFoundException extends RuntimeException{}
 }
--- core/src/main/java/zzleep/core/repositories/SleepRepositoryImpl.java
+++ core/src/main/java/zzleep/core/repositories/SleepRepositoryImpl.java
@@ -1,10 +1,8 @@
 package zzleep.core.repositories;
 
 import org.springframework.stereotype.Component;
-import zzleep.core.models.Device;
 import zzleep.core.models.Sleep;
 
-import javax.xml.crypto.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,86 +10,90 @@ import java.time.format.DateTimeFormatter;
 public class SleepRepositoryImpl implements SleepRepository {
 
     private Context context;
+    private final DeviceRepository deviceRepository;
 
     private static final Context.ResultSetExtractor<Sleep> extractor = ExtractorFactory.getSleepExtractor();
-    private static final Context.ResultSetExtractor<Device> deviceExtractor = ExtractorFactory.getDeviceExtractor();
 
-    public SleepRepositoryImpl(Context context) {
+    public SleepRepositoryImpl(Context context, DeviceRepository deviceRepository) {
         this.context = context;
+        this.deviceRepository = deviceRepository;
     }
 
     @Override
     public Sleep startTracking(String deviceId) throws SleepNotStoppedException, DeviceNotFoundException {
-        if(!checkDeviceExists(deviceId))
-        {
-            throw new DeviceNotFoundException();
-        }
-        Sleep sleep = context.single(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s = '%s' and %s is null", DatabaseConstants.SLEEP_COL_DEVICE_ID, deviceId, DatabaseConstants.SLEEP_COL_FINISH_TIME), extractor);
-        if (sleep != null) {
-            throw new SleepNotStoppedException();
-        } else
-            sleep = context.insert(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s, %s", DatabaseConstants.SLEEP_COL_DEVICE_ID, DatabaseConstants.SLEEP_COL_START_TIME), String.format("'%s', '%s'", deviceId, dateToString(LocalDateTime.now())), extractor);
-        return sleep;
+        if (!deviceRepository.exists(deviceId)) throw new DeviceNotFoundException();
+        if (getActiveSleep(deviceId) != null) throw new SleepNotStoppedException();
+        return createSleep(deviceId);
     }
 
     @Override
     public Sleep stopTracking(String deviceId) throws SleepNotStartedException, DeviceNotFoundException {
-        if(!checkDeviceExists(deviceId))
-        {
-            throw new DeviceNotFoundException();
-        }
-        Device device = context.single(DatabaseConstants.DEVICE_TABLE_NAME, String.format("%s = '%s'", DatabaseConstants.DEVICE_COL_ID, deviceId), deviceExtractor);
-        if(device == null)
-        {
-            throw new DeviceNotFoundException();
-        }
-        Sleep sleep = context.single(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s = '%s' and %s is null", DatabaseConstants.SLEEP_COL_DEVICE_ID, deviceId, DatabaseConstants.SLEEP_COL_FINISH_TIME), extractor);
-        if (sleep == null) {
-            throw new SleepNotStartedException();
-        } else
-            sleep = context.update(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s = '%s'", DatabaseConstants.SLEEP_COL_FINISH_TIME, dateToString(LocalDateTime.now())), String.format("%s = '%s'", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleep.getSleepId()), extractor);
-        return sleep;
+        if (!deviceRepository.exists(deviceId)) throw new DeviceNotFoundException();
+        Sleep sleep = getActiveSleep(deviceId);
+        if (sleep == null) throw new SleepNotStartedException();
+        return finishSleep(sleep.getSleepId());
     }
 
     @Override
     public boolean isTracking(String deviceId) {
-        if(!checkDeviceExists(deviceId))
-        {
-            return false;
-        }
-        Sleep sleep = context.single(
-                DatabaseConstants.SLEEP_TABLE_NAME,
-            String.format(
-                "%s = '%s' and %s is null",
-                    DatabaseConstants.SLEEP_COL_DEVICE_ID, deviceId, DatabaseConstants.SLEEP_COL_FINISH_TIME),
-            extractor
-        );
-
-        return sleep != null;
+        if (!deviceRepository.exists(deviceId)) throw new DeviceNotFoundException();
+        return getActiveSleep(deviceId) != null;
     }
 
     @Override
-    public Sleep rateSleep(String sleepId, int rating) throws SleepNotFoundException {
-        Sleep sleep = context.single(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s = '%s'", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleepId), extractor);
-        if (sleep == null) {
-            throw new SleepNotFoundException();
-        } else
-            sleep = context.update(DatabaseConstants.SLEEP_TABLE_NAME, String.format("%s = '%d'", DatabaseConstants.SLEEP_COL_RATING, rating), String.format("%s = '%s'", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleep.getSleepId()), extractor);
-        return sleep;
+    public Sleep rateSleep(int sleepId, int rating) throws SleepNotFoundException {
+        if (getById(sleepId) == null) throw new SleepNotFoundException();
+        return updateRating(sleepId, rating);
+    }
+
+    private Sleep createSleep(String deviceId) {
+        return context.insert(
+            DatabaseConstants.SLEEP_TABLE_NAME,
+            String.format("%s, %s", DatabaseConstants.SLEEP_COL_DEVICE_ID, DatabaseConstants.SLEEP_COL_START_TIME),
+            String.format("'%s', '%s'", deviceId, dateToString(LocalDateTime.now())),
+            extractor
+        );
+    }
+
+    private Sleep getById(int sleepId) {
+        return context.single(
+            DatabaseConstants.SLEEP_TABLE_NAME,
+            String.format("%s = %d", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleepId),
+            extractor);
+    }
+
+    private Sleep getActiveSleep(String deviceId) {
+        return context.single(
+            DatabaseConstants.SLEEP_TABLE_NAME,
+            String.format("%s = '%s' and %s is null",
+                DatabaseConstants.SLEEP_COL_DEVICE_ID, deviceId,
+                DatabaseConstants.SLEEP_COL_FINISH_TIME),
+            extractor
+        );
+    }
+
+    private Sleep updateRating(int sleepId, int rating) {
+        return context.update(
+            DatabaseConstants.SLEEP_TABLE_NAME,
+            String.format("%s = %d", DatabaseConstants.SLEEP_COL_RATING, rating),
+            String.format("%s = %d", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleepId),
+            extractor
+        );
+    }
+
+    private Sleep finishSleep(int sleepId) {
+        return context.update(
+            DatabaseConstants.SLEEP_TABLE_NAME,
+            String.format("%s = '%s'",
+                DatabaseConstants.SLEEP_COL_FINISH_TIME,
+                dateToString(LocalDateTime.now())),
+            String.format("%s = '%s'", DatabaseConstants.SLEEP_COL_SLEEP_ID, sleepId),
+            extractor
+        );
     }
 
     private String dateToString(LocalDateTime date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return date.format(formatter);
-    }
-
-    private boolean checkDeviceExists(String deviceId)
-    {
-        Device device = context.single(DatabaseConstants.DEVICE_TABLE_NAME, String.format("%s = '%s'", DatabaseConstants.DEVICE_COL_ID, deviceId), deviceExtractor);
-        if(device == null)
-        {
-            return false;
-        }
-        return true;
     }
 }

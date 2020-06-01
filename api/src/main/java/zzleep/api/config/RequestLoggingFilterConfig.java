8721b76c582d7a1895f56cf423dfbf56f5857c6f
mementox <sebestyen.mate98@gmail.com>
2020-06-01 12:15:51 +0200

    Fix error with preferences validation

--- api/src/main/java/zzleep/api/config/RequestLoggingFilterConfig.java
+++ api/src/main/java/zzleep/api/config/RequestLoggingFilterConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
-@Configuration
+// @Configuration
 public class RequestLoggingFilterConfig {
     @Bean
     public CommonsRequestLoggingFilter logFilter() {
